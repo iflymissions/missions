4d3949c78f57b0a1465025437bde22beff77d1db
iflymissions <84043558+iflymissions@users.noreply.github.com>
2021-06-05 17:30:33 -0400

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,7 +15,7 @@ const Projects = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/projects">Projects</Link></h2>
-          <p>A selection of projects that We&apos;ve been involved with</p>
+          <p>A selection of projects that I&apos;ve been involved with</p>
         </div>
       </header>
       {data.map((project) => (
