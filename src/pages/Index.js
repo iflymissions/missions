452f93c7f924843ff69704b4ba47ca9bc0001dc2
iflymissions <84043558+iflymissions@users.noreply.github.com>
2021-06-05 17:26:18 -0400

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,13 +6,13 @@ import Main from '../layouts/Main';
 const Index = () => (
   <Main
     description={"I Fly Missions's website. Savannah, GA,"
-    + 'Aerospace engineer now offering commercial drone services.'}
+    + 'Electronics engineer now offering commercial drone services.'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p> I Fly Missions is a professional drone service by an experienced aerospace engineer.
+          <p> I Fly Missions is a professional drone service by an experienced electronics engineer.
           </p>
         </div>
       </header>
